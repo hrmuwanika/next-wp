832f32961158d8a96fb3f66ed49ded4bfe871b14
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-06-28 17:00:28 +0200

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -27,11 +27,11 @@ const ToDelete = () => {
     <main className="space-y-6">
       <Prose>
         <h1>
-          <Balancer>Headless WordPress built with the Next.js</Balancer>
+          <Balancer>Headless WordPress + Next.js</Balancer>
         </h1>
 
         <p>
-          This is <a href="https://github.com/9d8dev/next-wp">next-wp</a>,
+          This is <a href="https://github.com/hrmuwanika/next-wp">next-wp</a>,
           created as a way to build WordPress sites with Next.js at rapid speed.
           This starter is designed with{" "}
           <a href="https://ui.shadcn.com">shadcn/ui</a>,{" "}
@@ -45,29 +45,7 @@ const ToDelete = () => {
 
       <div className="flex justify-between items-center gap-4">
         {/* Vercel Clone Starter */}
-        <div className="flex items-center gap-3">
-          <a
-            className="h-auto block"
-            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F9d8dev%2Fnext-wp&env=WORDPRESS_URL,WORDPRESS_HOSTNAME&envDescription=Add%20WordPress%20URL%20with%20Rest%20API%20enabled%20(ie.%20https%3A%2F%2Fwp.example.com)%20abd%20the%20hostname%20for%20Image%20rendering%20in%20Next%20JS%20(ie.%20wp.example.com)&project-name=next-wp&repository-name=next-wp&demo-title=Next%20JS%20and%20WordPress%20Starter&demo-url=https%3A%2F%2Fwp.9d8.dev"
-          >
-            {/* eslint-disable-next-line */}
-            <img
-              className="not-prose my-4"
-              src="https://vercel.com/button"
-              alt="Deploy with Vercel"
-              width={105}
-              height={32.62}
-            />
-          </a>
-          <p className="!text-sm sr-only sm:not-sr-only text-muted-foreground">
-            Deploy with Vercel in seconds.
-          </p>
-        </div>
-
-        <div className="flex gap-2 items-center">
-          <WordPressIcon className="text-foreground" width={32} height={32} />
-          <NextJsIcon className="text-foreground" width={32} height={32} />
-        </div>
+        
       </div>
 
       <div className="grid md:grid-cols-3 gap-4 mt-6">
